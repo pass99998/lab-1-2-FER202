16559e3fa979252689ae8413ef97a3d6a6e726c3
pass99998 <nhatbeo231@gmail.com>
2026-09-21 14:36:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-import  from './slot3/DemoProps';
-
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ProductCard from "./components/ProductCard";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const userList = [
-    { id: 1, name: "John", age: 20, address: "Ha Noi" },
-    { id: 2, name: "Loan", age: 22, address: "Da Nang" }
-  ];
-
-  function handleClick() {
-    alert("Button được chọn!");
-  }
-
-  return (
-    <div className="App p-3 container">
-      <DemoState1 />
-      <DemoState />
-      <DemoClass />
-      <User users={userList} onClick={handleClick} />
-      <DemoState2 users={userList} onclick={handleClick} />
-
-      <hr className="my-5" />
-
-      {/* Hiển thị Exercise 9 */}
-      <Exercise9 />
-
-      <hr className="my-5" />
-
-      {/* 2. Hiển thị Exercise 10 */}
-      <h2 className="fw-bold text-center mb-4 text-success">Exercise 10: Demo React-Bootstrap</h2>
-      <Exercise10 />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className="mt-4">
+                <Banner />
+            </div>
+            <div className="mt-4">
+                <ProductCard />
+            </div>
+            <div className="mt-4">
+                <Footer />
+            </div>
+        </div>
+    )
 }
+
 export default App;
