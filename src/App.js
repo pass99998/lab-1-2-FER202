e96ba28a85a29b22f9a8e75fe723e29bf4a462a9
pass99998 <nhatbeo231@gmail.com>
2026-09-21 14:20:07 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import  from './slot3/DemoProps';
+
 
 function App() {
+  const userList = [
+    { id: 1, name: "John", age: 20, address: "Ha Noi" },
+    { id: 2, name: "Loan", age: 22, address: "Da Nang" }
+  ];
+
+  function handleClick() {
+    alert("Button được chọn!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-3 container">
+      <DemoState1 />
+      <DemoState />
+      <DemoClass />
+      <User users={userList} onClick={handleClick} />
+      <DemoState2 users={userList} onclick={handleClick} />
+
+      <hr className="my-5" />
+
+      {/* Hiển thị Exercise 9 */}
+      <Exercise9 />
+
+      <hr className="my-5" />
+
+      {/* 2. Hiển thị Exercise 10 */}
+      <h2 className="fw-bold text-center mb-4 text-success">Exercise 10: Demo React-Bootstrap</h2>
+      <Exercise10 />
     </div>
   );
 }
-
 export default App;
---
+++ src/data/products.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    id: 1,
+    name: "Men Hoodie",
+    price: 450000,
+    status: "In Stock",
+    image: "C:\Users\Admin\Downloads\Images\nam4.jpg",
+  },
+  {
+    id: 2,
+    name: "Women T-Shirt",
+    price: 250000,
+    status: "In Stock",
+    image: "C:\Users\Admin\Downloads\Images\nu4.jpg",
+  },
+  {
+    id: 3,
+    name: "Jeans",
+    price: 500000,
+    status: "In Stock",
+    image: "C:\Users\Admin\Downloads\Images\nam2.jpg",
+  },
+  {
+    id: 4,
+    name: "Jacket",
+    price: 650000,
+    status: "In Stock",
+    image:"C:\Users\Admin\Downloads\Images\nam3.jpg" ,
+  },
+  {
+    id: 5,
+    name: "Dress",
+    price: 550000,
+    status: "In Stock",
+    image: "C:\Users\Admin\Downloads\Images\Nu2.jpg",
+  },
+  {
+    id: 6,
+    name: "Shirt",
+    price: 350000,
+    status: "In Stock",
+    image: "C:\Users\Admin\Downloads\Images\nam1.jpg",
+  },
+];
+export default products;
